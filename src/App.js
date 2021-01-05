8f6db11d6cc4799a0c9a7add293a455a2c6c6f94
justmaster <tornike-10@yandex.ru>
2021-01-06 00:41:44 +0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+  <div className="header"></div>
+    <div className="container">
+        <nav className="nav">
+            <ul className="nav-list nav-list-mobile">
+              <li className="nav-item">
+                <div className="mobile-menu">
+                    <span className="line line-top"></span>
+                    <span className="line line-bottom"></span>
+                </div>
+              </li>
+              <li className="nav-item">
+                <a href="index.html" className="nav-link nav-link-apple"></a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link-bag"></a>
+              </li>
+            </ul>
+            {/* <nav-list nav-list-mobile> */}
+            <ul className="nav-list nav-list-larger">
+              <li className="nav-item">
+                <a href="index.html" className="nav-link nav-link-apple"></a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link">У</a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link">Геги</a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link">Не</a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link">Пошёл</a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link">Киберпанк</a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link">Ха</a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-link">Ха</a>
+              </li>
+              <li className="nav-item">
+                <a href='#' className="nav-link nav-search"></a>
+              </li>
+              <li className="nav-item nav-item-hidden">
+                <a href='#' className="nav-link nav-link-bag"></a>
+              </li>
+            </ul>
+            {/* <nav-list nav-list-larger> */}
+        </nav>
+    </div>
+
+    <div className="hero iphone-11-pro">
+      <div className="container">
+        <div className="title">
+          <h2 className="title-heading">Новый Колбасный Цех</h2>
+          <h3 className="title-sub-heading">Pro cameras. Pro display. Pro performance.</h3>
+          <p className="title-price">From $24.95/mo. or $599 with trade-in Детский садик</p>
+        </div>
+        <div className="cta">
+          <a href="#" className="cta-link">Learn More</a>
+          <a href="#" className="cta-link">Buy</a>
+        </div>
+      </div>
+    </div>
+
+    <div className="hero iphone-11">
+      <div className="container">
+        <div className="title title-dark">
+          <h2 className="title-heading">Iphone 11 Pro</h2>
+          <h3 className="title-sub-heading">Just the right amount of everything</h3>
+          <p className="title-price">From $16.62/mo. or $399 with trade-in</p>
+        </div>
+        <div className="cta">
+          <a href="#" className="cta-link cta-link darker">Learn More</a>
+          <a href="#" className="cta-link cta-link darker">Buy</a>
+        </div>
+      </div>
+    </div>
+
+    <div className="hero watch">
+      <div className="container">
+        <div className="title title-dark">
+          <h2 className="title-heading-watch"></h2>
+          <h3 className="title-sub-heading">With the new Alway On Retina Display
+                You've never seen a watch like this</h3>
+        </div>
+        <div className="cta">
+          <a href="#" className="cta-link cta-link darker">Learn More</a>
+          <a href="#" className="cta-link cta-link darker">Buy</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  );
+}
+
+export default App;
